434622f186c06e29b563a86e1cc24de6a8842a57
babicat <61409571+babicat@users.noreply.github.com>
2020-03-06 21:10:49 +0900

    Update MapReduce.py

--- study/MapReduce.py
+++ study/MapReduce.py
@@ -1,8 +1,12 @@
 '''
-#MapReduce 작동방식 (이해하기 어렵다.)
+#1. MapReduce 작동방식 (이해하기 어렵다... sql 관련 글이었군)
 
 https://m.blog.naver.com/jevida/140200008968
 
+
+#2. MapReduce의 파이썬에서의 구동 방식
+
+https://3months.tistory.com/521
 
 
 
