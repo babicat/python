80356e6e51492d6cd9f6d972c367ca73a8b1c995
Sona <seoyeong@treenod.com>
2020-07-19 16:09:49 +0900

    language check

--- language_check.py
+++ language_check.py
@@ -16,62 +16,31 @@ def check(string):
     en = english.findall(string)
     ja = japanese.findall(string)
 
-    #lang_len = {'k': len(ko), 'e': len(en), 'j': len(ja)}
-    lang_len = [len(ko), len(en), len(ja)]
-    lang_dict = {'Korean': len(ko), 'English': len(en), 'Japanese': len(ja)}
+    lang = {'Korean': len(ko), 'English': len(en), 'Japanese': len(ja)}
 
-    length = len(set(lang_len))
+    length = len(set(lang.values()))
 
+    #모두 동일하지 않으면
     if length != 1:
-
-        max_num = max(lang_len)
-        for i, j in lang_dict.items():
+        print("비중이 높은 언어: ", end=' ')
+        #제일 큰 길이 구하기
+        max_num = max(lang.values())
+        for i, j in lang.items():
+            #제일 큰 길이면
             if j == max_num:
+                #해당 key 출력
                 print(i, end=' ')
-        a = percentage(max_num, lang_len)
+        ratio = percentage(max_num, lang.values())
+        print('(', ratio, '% )')
+
+    #모두 동일하면
+    elif length == 1:
+        print("모든 언어의 비중이 동일합니다")
+    #판별 불가한 언어면
+    else:
+        print("언어 판별이 불가능 합니다.")
 
     return None
 
-
 string = 'I am a 한국 ひと 입니다.'
-
 result = check(string)
-#a = percentage(result)
-#print(a)
-#print(result)
-#print(result[0][0])
-
-'''
-if len(set(result)) == 3:
-    p = percentage(result)
-
-    print("대상 문자열: ", string)
-
-    if result[0] == k_len:
-        print("한국어 비중이", result, "%로 가장 높습니다.")
-    elif max == e_len:
-        print("영어 비중이", result, "%로 가장 높습니다.")
-    elif max == j_len:
-        print("일본어 비중이", result, "%로 가장 높습니다.")
-
-elif len(set(lang_len)) == 2:
-    max = max(lang_len)
-    sum = len(k) + len(e) + len(j)
-    result = percentage(max, sum)
-
-    print("대상 문자열: ", string)
-
-    if (max == len(k)) & (max == len(e)):
-        print("한국어, 영어 비중이 각", result, "%로 가장 높습니다.")
-    elif (max == len(k)) & (max == len(j)):
-        print("한국어, 일본어 비중이 각", result, "%로 가장 높습니다.")
-    elif (max == len(e)) & (max == len(j)):
-        print("영어, 일본어 비중이 각", result, "%로 가장 높습니다.")
-
-elif len(set(lang_len)) == 1:
-    print("대상 문자열: ", string)
-    print("모든 언어의 비중이 동일합니다.")
-
-else:
-    print("언어 판별이 불가능 합니다.")
-'''
